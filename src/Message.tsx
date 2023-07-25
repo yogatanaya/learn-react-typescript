7e1e59da5e4268e6add84bf3d54e1beca1769181
yogat@iseka.services <yogat@iseka.services>
2023-07-25 11:45:12 +0800

    initial

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface UserMessage {
+    name: string,
+    message: string
+}
+
+const Message = (props: UserMessage): any => {
+    return (<p>{props.name}, {props.message}</p>)
+}
+
+export default Message;
